9e09bf1aed13c94f33c3731da1517f71849ce93e
Marco Ruiz <marco.ruiz.di@gmail.com>
2024-10-15 18:27:40 -0300

    variables interpoladas

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import carnet from '@/components/carnet.vue'; //se importa carnet.vue
+
+export default {
+  name: 'app',
+  components: {
+    carnet
+  }
+
+}
+</script>
+<template>
+
+  <carnet /> 
+</template>
